0758be374a57a8a9e7bb6004abe1ec0e36f05c72
Pier Salloum <piersalloum@gmail.com>
2023-03-29 00:50:10 -0700

    working timer, updates to js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,67 @@
-const startButton = document.getElementById("start")
+//code to remove start and show game from hidden visibility
+    let toggleGame = document.getElementById("game");
+    let playHide = document.getElementById("startgame");
 
-startButton.addEventListener("click", startGame)
+    playHide.addEventListener("click", function() {
+        toggleGame.style.visibility = "visible";
+        if (toggleGame.style.visibility === "visible") {
+          playHide.remove();
+        }
+      });
+//code to remove start and show game from hidden visibility
 
-function startGame() {
-    console.log("hola")
-}
-function setNextQuestion() {
+//code for timer
+    let timerGame = document.getElementById("timer");
+    let timerTimeLeft = 5;
+    let intervalId = null
+
+    playHide.addEventListener("click", myTimer)
+      intervalId = setInterval(myTimer, 1000);
+
+    function myTimer() {
+      if (timerTimeLeft <= 0) {
+        clearInterval(intervalId);
+        timerGame.innerHTML = "Time's up!";
+      }
+      else if (toggleGame.style.visibility === "visible") {
+        timerTimeLeft--;
+        timerGame.innerHTML = timerTimeLeft;
+      }
+    }
+  //code for timer
+
+  //array of questions
+  const questions = [
+    {
+      question: "what is 2+2",
+      answers:  {
+        answer1: "2",
+        answer2: "8",
+        answer3: "7",
+        answer4: "4"
+      },
+      correctAnswer: "answer4"   
+    },
+    {
+      question: "what is 2*2",
+      answers:  {
+        answer1: "2",
+        answer2: "8",
+        answer3: "7",
+        answer4: "4"
+      },
+      correctAnswer: "answer4"   
+    }
     
-}
-function selectAnswer() {
-    
-}
+  ];
+  //array of questions
+
+    let questionDisplay = document.getElementById("questions");
+
+    if (toggleGame.style.visibility === "visible"); {
+      questionDisplay.innerHTML = 
+    }
+
+
+
+      
